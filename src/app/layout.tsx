16bfef5d662232e457efcb68763ea204a8b1f04d
Dummy Mail <dummymaill123@gmail.com>
2026-08-20 12:34:38 +0000

    Try fixing this error: `Runtime SyntaxError: Failed to execute 'appendCh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,14 @@ export default function RootLayout({
 }>) {
   const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   const gtmId = process.env.NEXT_PUBLIC_GTM_MEASUREMENT_ID;
+  
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3474831473342898"
@@ -31,7 +33,7 @@ export default function RootLayout({
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
         />
-        <Script id="ga-script" strategy="afterInteractive">
+        <Script id="ga-config-script" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -40,20 +42,19 @@ export default function RootLayout({
           `}
         </Script>
 
-        
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DSRKTMS6XZ">
-        </Script>
-        <Script>
+        <Script 
+          async 
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtmId}`}
+        />
+        <Script id="gtm-config-script" strategy="afterInteractive">
           {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${gtmId});
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtmId}');
           `}
         </Script>
-
-
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         {children}
